77a0c5059c1de44288c77a507c466dc9ee60e6b9
Aidan Gannon <aidanwilliamgannon@gmail.com>
2025-11-22 17:52:38 +0000

    Add lua keymaps

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -34,20 +34,9 @@ require("lazy").setup({
       vim.lsp.enable('ts_ls')
       vim.lsp.enable('pyright')
       
-      vim.api.nvim_create_autocmd('LspAttach', {
-        callback = function(args)
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = args.buf })
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = args.buf })
-          vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { buffer = args.buf })
-          vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { buffer = args.buf })
-          vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { buffer = args.buf })
-          vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { buffer = args.buf })
-          vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { buffer = args.buf })
-        end,
-      })
+      vim.api.nvim_create_autocmd('LspAttach', require('config.lsp-keymaps'))
     end,
   },
-
   {
     'hrsh7th/nvim-cmp',
     dependencies = {
